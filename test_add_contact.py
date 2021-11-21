548fbc07f2ab8fdf129bdab39048ca39a49dca9b
sash <softester@yandex.ru>
2021-11-21 22:01:51 +0300

    Added pytest framework and fixtures

--- test_add_contact.py
+++ test_add_contact.py
@@ -1,53 +1,46 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import NoAlertPresentException
-import unittest
+import pytest
 from contact import Contact
 from application import Application
 
 
-class UntitledTestCase(unittest.TestCase):
-
-    def setUp(self):
-        self.app = Application()
-
-    def test_add_contact(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_contact(Contact(firstname="FirstName",
-                                        middlename="MiddleName",
-                                        lastname="LastName",
-                                        nickname="NickName",
-                                        company="Company",
-                                        title="Ttile",
-                                        address="Address",
-                                        home="1001",
-                                        mobile="1002",
-                                        work="1003",
-                                        fax="1004",
-                                        email="email@email.email",
-                                        email2="email2@email2.email2",
-                                        email3="email3@email3.email3",
-                                        homepage="Homepage",
-                                        address2="Address2",
-                                        phone2="1005",
-                                        notes="Notes",
-                                        bday="5",
-                                        bmonth="May",
-                                        byear="1950",
-                                        aday="15",
-                                        amonth="March",
-                                        ayear="1955"))
-        self.app.logout()
-
-    def test_add_empty_contact(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_contact(Contact(firstname=""))
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_contact(app):
+    app.login(username="admin", password="secret")
+    app.create_contact(Contact(firstname="FirstName",
+                                    middlename="MiddleName",
+                                    lastname="LastName",
+                                    nickname="NickName",
+                                    company="Company",
+                                    title="Ttile",
+                                    address="Address",
+                                    home="1001",
+                                    mobile="1002",
+                                    work="1003",
+                                    fax="1004",
+                                    email="email@email.email",
+                                    email2="email2@email2.email2",
+                                    email3="email3@email3.email3",
+                                    homepage="Homepage",
+                                    address2="Address2",
+                                    phone2="1005",
+                                    notes="Notes",
+                                    bday="5",
+                                    bmonth="May",
+                                    byear="1950",
+                                    aday="15",
+                                    amonth="March",
+                                    ayear="1955"))
+    app.logout()
+
+
+def test_add_empty_contact(app):
+    app.login(username="admin", password="secret")
+    app.create_contact(Contact(firstname=""))
+    app.logout()
--- test_add_group.py
+++ test_add_group.py
@@ -1,26 +1,23 @@
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
 from group import Group
 from application import Application
 
 
-class UntitledTestCase(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-
-    def test_add_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="GroupName", header="GroupHeader", footer="GroupFooter"))
-        self.app.logout()
-
-    def test_add_empty_group(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="", header="", footer=""))
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_add_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="GroupName", header="GroupHeader", footer="GroupFooter"))
+    app.logout()
+
+
+def test_add_empty_group(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
