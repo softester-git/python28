c598a307eb175d575bba71f05cc830045a010b5e
sash <softester@yandex.ru>
2021-11-26 08:56:05 +0300

    Added check for delete contact

--- fixture/contact.py
+++ fixture/contact.py
@@ -78,3 +78,8 @@ class ContactHelper:
         if text is not None:
             wd.find_element_by_name(field_name).click()
             Select(wd.find_element_by_name(field_name)).select_by_visible_text(text)
+
+    def count(self):
+        wd = self.app.wd
+        self.app.open_home_page()
+        return len(wd.find_elements_by_name("selected[]"))
