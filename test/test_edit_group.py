4d479141b323526457a5dce9fcb0d8f5f5dd05df
Sash <softester@yandex.ru>
2021-12-05 14:52:10 +0300

    Added lists assertion for test_edit_group

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,4 +4,11 @@ from model.group import Group
 def test_edit_first_group(app):
     if app.group.count() == 0:
         app.group.create(Group(name="Group for edit"))
-    app.group.edit_first(Group(name="new_group_name", header="new_group_header", footer="new_group_footer"))
+    old_groups = app.group.get_group_list()
+    group = Group(name="new_group_name", header="new_group_header", footer="new_group_footer")
+    group.id = old_groups[0].id
+    app.group.edit_first(group)
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
