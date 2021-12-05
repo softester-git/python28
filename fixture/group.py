8ae38b2cf467d4b4b6f6e9a168b9234875210357
Sash <softester@yandex.ru>
2021-12-05 14:43:08 +0300

    Added lists assertion for test_add_group

--- fixture/group.py
+++ fixture/group.py
@@ -1,4 +1,4 @@
-from selenium.webdriver.support.select import Select
+from model.group import Group
 
 
 class GroupHelper:
@@ -65,3 +65,13 @@ class GroupHelper:
         wd = self.app.wd
         self.open_groups_page()
         return len(wd.find_elements_by_name("selected[]"))
+
+    def get_group_list(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        list_group = []
+        for element in wd.find_elements_by_css_selector("span.group"):
+            text = element.text
+            id = element.find_element_by_name("selected[]").get_attribute("value")
+            list_group.append(Group(name=text, id=id))
+        return(list_group)
