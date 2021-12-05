968ea1be8108109e94ab57c7e05c79689a883ea8
Sash <softester@yandex.ru>
2021-12-05 15:14:05 +0300

    Added lists assertions for contacts and groups

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -4,4 +4,8 @@ from model.contact import Contact
 def test_delete_first_contact(app):
     if app.contact.count() == 0:
         app.contact.create(Contact(firstname="Contact for delete"))
+    old_contacts = app.contact.get_contact_list()
     app.contact.delete_first_contact()
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) - 1 == len(new_contacts)
+    assert old_contacts[1:] == new_contacts
