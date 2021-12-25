46360d2101094c8590c52388011e637a6806d275
Sash <softester@yandex.ru>
2021-12-25 22:33:01 +0300

    Added db checks

---
+++ fixture/db.py
@@ -0,0 +1,45 @@
+import mysql.connector
+
+from model.contact import Contact
+from model.group import Group
+
+
+class DbFixture:
+
+    def __init__(self, host, name, user, password):
+        self.host = host
+        self.name = name
+        self.user = user
+        self.password = password
+        self.connection = mysql.connector.connect(host=host, database=name, user=user, password=password,
+                                                  autocommit=True)
+
+    def get_group_list(self):
+        group_list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute("select group_id, group_name, group_header, group_footer from group_list")
+            for row in cursor:
+                (id, name, header, footer) = row
+                group_list.append(Group(id=str(id), name=name.strip(), header=header, footer=footer))
+        finally:
+            cursor.close()
+        return (group_list)
+
+    def get_contact_list(self):
+        contact_list = []
+        cursor = self.connection.cursor()
+        try:
+            cursor.execute(
+                "select id, firstname, lastname, address from addressbook where deprecated='0000-00-00 00:00:00'")
+            for row in cursor:
+                (id, firstname, lastname, address) = row
+                contact_list.append(
+                    Contact(id=str(id), firstname=" ".join(firstname.split()), lastname=" ".join(lastname.split()),
+                            address=" ".join(address.split())))
+        finally:
+            cursor.close()
+        return (contact_list)
+
+    def destroy(self):
+        self.connection.close()
